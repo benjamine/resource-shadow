5611fddf33fb45b68df6962763014f2c460de28b
Benjamin Eidelman <beneidel@gmail.com>
2014-11-14 09:58:50 -0300

    0.0.9

--- public/build/resource-shadow.js
+++ public/build/resource-shadow.js
@@ -9,7 +9,7 @@ exports.create = ResourceShadow.create;
 
 if (process.browser) {
   // exports only for browser bundle
-  exports.version = '0.0.8';
+  exports.version = '0.0.9';
   exports.homepage = 'https://github.com/benjamine/resource-shadow';
 } else {
   // exports only for node.js
